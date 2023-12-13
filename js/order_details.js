1de37a61b75bd678835551af53ebfa8bee32a5c6
minhtuanle301202 <Minhtuan8d@gmail.com>
2023-12-13 17:42:27 +0700

    drop-down-profile

--- js/order_details.js
+++ js/order_details.js
@@ -16,3 +16,24 @@ linkTo_user_information.addEventListener('click', () => {
 linkTo_exit.addEventListener('click',() => {
   window.location.href = "index.html"
 })
+
+let user_picture = document.querySelector('.user-pic') ;
+      let menu = document.querySelector('.sub-menu-wrap') ;
+      user_picture.addEventListener('click',() => {
+         menu.classList.toggle('open-menu') ;
+      })
+let user_information = document.querySelector('.user-information') ;
+let user_order = document.querySelector('.user-order') ;
+let user_logout = document.querySelector('.user-logout') ;
+
+user_order.addEventListener('click', () => {
+   window.location.href="order_details.html"  ; 
+})
+
+user_information.addEventListener('click', () => {
+  window.location.href="user_information.html"  ; 
+})
+
+user_logout.addEventListener('click', () => {
+  window.location.href="index.html"  ; 
+})
